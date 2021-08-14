bd2ba7f23580f69b15f328d5b876a50c4888b3f5
llaannn <lianhsih@gmail.com>
2021-08-14 16:51:53 +0800

    登入頁重構完成

--- AccountNoteFin/Login.aspx.cs
+++ AccountNoteFin/Login.aspx.cs
@@ -1,4 +1,5 @@
-﻿using AccountingNote.DBSource;
+﻿using AccountingNote.Auth;
+using AccountingNote.DBSource;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -34,38 +35,47 @@ namespace AccountNoteFin
             string inp_Acc = this.txtAcc.Text;
             string inp_PWD = this.txtPWD.Text;
 
-
-            if (string.IsNullOrWhiteSpace(inp_Acc) || string.IsNullOrWhiteSpace(inp_PWD))
+            string msg;
+            if (!AuthManager.TryLogin(inp_Acc, inp_PWD, out msg))
             {
-                this.ltlMsg.Text = "帳號或密碼為空白";
+                this.ltlMsg.Text = msg;
                 return;
             }
-
-            var dr = UserInfoManger.GetUserInfoByAccount(inp_Acc);
-
-            //if (string.Compare(db_acc, inp_Acc, true) == 0 && string.Compare(db_pwd, inp_Pwd, false)==0)
-
-            if (dr == null)
-            {
-                this.ltlMsg.Text = "輸入的帳號並不存在";
-                return;
-            }
+            Response.Redirect("/SystemAdmin/UserInfo.aspx");
 
 
-            if (string.Compare(dr["Account"].ToString(), inp_Acc, true) == 0 && string.Compare(dr["PWD"].ToString(), inp_PWD, false) == 0)
-            {
-                //this.Session["UserLoginInfo"] = db_acc;
-                this.Session["UserLoginInfo"] = dr["Account"].ToString();
-                Response.Redirect("/SystemAdmin/UserInfo.aspx");
+            //    if (string.IsNullOrWhiteSpace(inp_Acc) || string.IsNullOrWhiteSpace(inp_PWD))
+            //    {
+            //        this.ltlMsg.Text = "帳號或密碼為空白";
+            //        return;
+            //    }
+
+            //    var dr = UserInfoManger.GetUserInfoByAccount(inp_Acc);
+
+            //    //if (string.Compare(db_acc, inp_Acc, true) == 0 && string.Compare(db_pwd, inp_Pwd, false)==0)
+
+            //    if (dr == null)
+            //    {
+            //        this.ltlMsg.Text = "輸入的帳號並不存在";
+            //        return;
+            //    }
 
 
-            }
+            //    if (string.Compare(dr["Account"].ToString(), inp_Acc, true) == 0 && string.Compare(dr["PWD"].ToString(), inp_PWD, false) == 0)
+            //    {
+            //        //this.Session["UserLoginInfo"] = db_acc;
+            //        this.Session["UserLoginInfo"] = dr["Account"].ToString();
+            //        Response.Redirect("/SystemAdmin/UserInfo.aspx");
 
-            else
-            {
-                this.ltlMsg.Text = "登入失敗，請確定輸入正確的帳號及密碼";
-                return;
-            }
+
+            //    }
+
+            //    else
+            //    {
+            //        this.ltlMsg.Text = "登入失敗，請確定輸入正確的帳號及密碼";
+            //        return;
+            //    }
+            //}
         }
     }
     
--- AccountingNote.Auth/AuthManager.cs
+++ AccountingNote.Auth/AuthManager.cs
@@ -49,10 +49,50 @@ namespace AccountingNote.Auth
             return model;
         }
 
+        /// <summary>
+        /// 登出
+        /// </summary>
         public static void Logout()
         {
             HttpContext.Current.Session["UserLoginInfo"] = null;
            
+        }
+
+
+        public static bool TryLogin(string account,string pwd,out string errorMsg)
+        {
+            //檢查輸入內容是否為空字串
+            if (string.IsNullOrWhiteSpace(account) || string.IsNullOrWhiteSpace(pwd))
+            {
+                errorMsg = "帳號或密碼為空白";
+                return false;
+            }
+
+            //讀取資料庫查找帳號
+            var dr = UserInfoManger.GetUserInfoByAccount(account);
+
+            if (dr == null)
+            {
+                errorMsg = $"輸入的帳號:{account}並不存在";
+                return false;
+            }
+
+
+            if (string.Compare(dr["Account"].ToString(), account, true) == 0 && string.Compare(dr["PWD"].ToString(), pwd, false) == 0)
+            {
+                //this.Session["UserLoginInfo"] = db_acc;
+                HttpContext.Current.Session["UserLoginInfo"] = dr["Account"].ToString();
+                errorMsg = string.Empty;
+                return true;
+
+
+            }
+
+            else
+            {
+                errorMsg = "登入失敗，請確定輸入正確的帳號及密碼";
+                return false;
+            }
         }
         
     }
